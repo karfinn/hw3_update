07702b9ffdf1f405823f054ba87598d06668c3dd
Karina Finn <karina.finn@kellogg.northwestern.edu>
2024-02-16 00:36:29 +0000

    progress

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,21 @@
 class PlacesController < ApplicationController
+
+  def index
+    @places = Place.all
+  end
+
+  def show
+    @places= Place.find_by({ "id" => params["id"] })
+  end 
+
+  def create
+    @places = Place.new
+    @places["name"] = params["name"]
+    @places.save
+  end 
+
 end
 
-def index
-  @places = Place.all
-end
+
 
 
